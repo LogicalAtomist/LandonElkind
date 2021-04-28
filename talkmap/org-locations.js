4fdc36068e162eee342ee37e8f68052a35566cea
Landon D. C. Elkind <35347800+LogicalAtomist@users.noreply.github.com>
2021-04-27 21:33:51 -0600

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,13 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Portland OR, USA",
+    45.5051,
+    -122.6750
+  ],
+  [
+    "New York NY, USA",
+    40.7128,
+    -74.0060
   ],
   [
     "London, UK",
@@ -10,7 +15,7 @@ var addressPoints = [
     -0.1276473
   ],
   [
-    "San Francisco, California",
+    "San Francisco, CA",
     37.7792808,
     -122.4192362
   ],
@@ -19,4 +24,19 @@ var addressPoints = [
     34.0543942,
     -118.2439408
   ]
+  [
+    "Memphis TN, USA",
+    35.1495,
+    -90.0490
+  ],
+  [
+    "Iowa City IA, USA",
+    41.6611,
+    -91.5302
+  ],
+  [
+    "Delhi, India",
+    41.6611,
+    77.1025
+  ],
 ];
